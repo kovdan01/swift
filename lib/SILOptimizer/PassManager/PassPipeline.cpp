19003560739f27ead3ab7a6627b722da9e728f61
Daniil Kovalev <daniil@kovalev.website>
2025-04-01 23:03:07 +0300

    v2: Support multi-bb functions in AutoDiff Closure Specialization pass

--- lib/SILOptimizer/PassManager/PassPipeline.cpp
+++ lib/SILOptimizer/PassManager/PassPipeline.cpp
@@ -1014,13 +1014,13 @@ SILPassPipelinePlan::getPerformancePassPipeline(const SILOptions &Options) {
   // Strip any transparent functions that still have ownership.
   P.addOwnershipModelEliminator();
 
-  P.addAutodiffClosureSpecialization();
+  P.addAutodiffClosureSpecialization1();
 
   // After serialization run the function pass pipeline to iteratively lower
   // high-level constructs like @_semantics calls.
   addMidLevelFunctionPipeline(P);
 
-  P.addAutodiffClosureSpecialization();
+  P.addAutodiffClosureSpecialization2();
 
   // Perform optimizations that specialize.
   addClosureSpecializePassPipeline(P);
