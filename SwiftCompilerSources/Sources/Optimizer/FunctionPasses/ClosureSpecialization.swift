4583c20497e5245072d60a4d0a2daa7b07f7563f
Daniil Kovalev <dkovalev@accesssoftek.com>
2025-05-24 00:01:12 +0300

    Fix crash

--- SwiftCompilerSources/Sources/Optimizer/FunctionPasses/ClosureSpecialization.swift
+++ SwiftCompilerSources/Sources/Optimizer/FunctionPasses/ClosureSpecialization.swift
@@ -670,7 +670,7 @@ private func getOrCreateSpecializedFunctionCFG(
         }
       }
     }
-    if currentEnumTypeOpt != nil {
+    if currentEnumTypeOpt != nil && !enumTypesReverseQueue.contains(currentEnumTypeOpt!) {
       enumTypesReverseQueue.append(currentEnumTypeOpt!)
     }
     for succ in block.successors {
