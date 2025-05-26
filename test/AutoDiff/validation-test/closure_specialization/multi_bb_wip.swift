f96820a6b2ba7698f5b8e4de9b5a54a4f8b22b12
Daniil Kovalev <dkovalev@accesssoftek.com>
2025-05-26 14:52:12 +0300

    remove vjp bb to tulpe map

--- test/AutoDiff/validation-test/closure_specialization/multi_bb_wip.swift
+++ test/AutoDiff/validation-test/closure_specialization/multi_bb_wip.swift
@@ -12,8 +12,8 @@
 
 /// Particular optimizations are checked in SILOptimizer tests, here we only check that optimizations occur
 // RUN: %target-swift-frontend -emit-sil -I%t %s -o - -O | %FileCheck %s
-/// TODO: support myfoo7. Now unsupported because of unexpected location of payload tuple instructions in vjp
 // CHECK-NONE: {{^}}// pullback of myfoo8
+// CHECK-NONE: {{^}}// pullback of myfoo7
 // CHECK-NONE: {{^}}// pullback of myfoo6
 // CHECK-NONE: {{^}}// pullback of myfoo5
 // CHECK-NONE: {{^}}// pullback of myfoo4
@@ -21,6 +21,7 @@
 // CHECK-NONE: {{^}}// pullback of myfoo2
 // CHECK-NONE: {{^}}// pullback of myfoo1
 // CHECK:      {{^}}// specialized pullback of myfoo8
+// CHECK:      {{^}}// specialized pullback of myfoo7
 // CHECK:      {{^}}// specialized pullback of myfoo6
 // CHECK:      {{^}}// specialized pullback of myfoo5
 // CHECK:      {{^}}// specialized pullback of myfoo4
