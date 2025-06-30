508770fa956523c0ebf05bcb323a796755b8d646
Daniil Kovalev <dkovalev@accesssoftek.com>
2025-06-30 17:03:00 +0300

    second test for begin_borrow

--- test/AutoDiff/validation-test/closure_specialization/multi_bb_wip.swift
+++ test/AutoDiff/validation-test/closure_specialization/multi_bb_wip.swift
@@ -55,6 +55,89 @@ import closure_spec_module
 
 var AutoDiffClosureSpecializationTests = TestSuite("AutoDiffClosureSpecialization")
 
+extension Dictionary: Differentiable where Value: Differentiable {
+    public typealias TangentVector = [Key: Value.TangentVector]
+    public mutating func move(by direction: TangentVector) {
+        for (componentKey, componentDirection) in direction {
+            func fatalMissingComponent() -> Value {
+                fatalError("missing component \(componentKey) in moved Dictionary")
+            }
+            self[componentKey, default: fatalMissingComponent()].move(by: componentDirection)
+        }
+    }
+
+    public var zeroTangentVectorInitializer: () -> TangentVector {
+        let listOfKeys = self.keys // capturing only what's needed, not the entire self, in order to not waste memory
+        func initializer() -> Self.TangentVector {
+            return listOfKeys.reduce(into: [Key: Value.TangentVector]()) { $0[$1] = Value.TangentVector.zero }
+        }
+        return initializer
+    }
+}
+
+extension Dictionary: AdditiveArithmetic where Value: AdditiveArithmetic {
+    public static func + (_ lhs: Self, _ rhs: Self) -> Self {
+        return lhs.merging(rhs, uniquingKeysWith: +)
+    }
+
+    public static func - (_ lhs: Self, _ rhs: Self) -> Self {
+        return lhs.merging(rhs.mapValues { .zero - $0 }, uniquingKeysWith: +)
+    }
+
+    public static var zero: Self { [:] }
+}
+
+extension Dictionary where Value: Differentiable {
+    // get
+    @usableFromInline
+    @derivative(of: subscript(_:))
+    func vjpSubscriptGet(key: Key) -> (value: Value?, pullback: (Optional<Value>.TangentVector) -> Dictionary<Key, Value>.TangentVector) {
+        // When adding two dictionaries, nil values are equivalent to zeroes, so there is no need to manually zero-out
+        // every key's value. Instead, it is faster to create a dictionary with the single non-zero entry.
+        return (self[key], { v in
+            if let value = v.value {
+                return [key: value]
+            }
+            else {
+                return .zero
+            }
+        })
+    }
+ }
+
+public extension Dictionary where Value: Differentiable {
+    @differentiable(reverse)
+    mutating func set(_ key: Key, to newValue: Value) {
+        self[key] = newValue
+    }
+
+    @derivative(of: set)
+    mutating func vjpUpdated(_ key: Key, to newValue: Value) -> (value: Void, pullback: (inout TangentVector) -> (Value.TangentVector)) {
+        self.set(key, to: newValue)
+
+        let forwardCount = self.count
+        let forwardKeys = self.keys // may be heavy to capture all of these, not sure how to do without them though
+
+        return ((), { v in
+            // manual zero tangent initialization
+            if v.count < forwardCount {
+                v = Self.TangentVector()
+                forwardKeys.forEach { v[$0] = .zero }
+            }
+
+            if let dElement = v[key] {
+                v[key] = .zero
+                return dElement
+            }
+            else { // should this fail?
+                v[key] = .zero
+                return .zero
+            }
+        })
+    }
+}
+
+
 AutoDiffClosureSpecializationTests.testWithLeakChecking("Test") {
   func myfoo01(_ x: Float) -> Float {
     if x > 0 {
@@ -450,6 +533,20 @@ AutoDiffClosureSpecializationTests.testWithLeakChecking("Test") {
   expectEqual((8, 2), valueWithGradient(at: 4, of: myfoo15))
   expectEqual((-20, 2), valueWithGradient(at: -10, of: myfoo15))
   expectEqual((-2674, 2), valueWithGradient(at: -1337, of: myfoo15))
+
+  func getD(from newValues: [String: Double], at key: String) -> Double? {
+    if newValues.keys.contains(key) {
+      return newValues[key]
+    }
+    return nil
+  }
+
+  @differentiable(reverse)
+  func testFunctionD(newValues: [String: Double]) -> Double {
+    return getD(from: newValues, at: "s1")!
+  }
+
+  expectEqual(pullback(at: ["s1": 1.0], of: testFunctionD)(2), ["s1" : 2.0])
 }
 
 runAllTests()
