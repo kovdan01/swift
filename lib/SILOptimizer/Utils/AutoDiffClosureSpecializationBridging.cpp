d389d37313f07a89b528c3b626df7ac972446dd3
Daniil Kovalev <dkovalev@accesssoftek.com>
2025-10-06 01:01:37 +0300

    [AutoDiff] Closure specialization: specialize branch tracing enums
    This patch contains part of the changes intended to resolve #68944.
    
    1. Closure info gathering logic.
    
    2. Branch tracing enum specialization logic.
    
    3. Specialization of branch tracing enum basic block arguments in VJP.
    
    4. Specialization of branch tracing enum payload basic block arguments
       in pullback.
    
    5. C++ helpers for things which are not yet bridged to Swift, such as
       demangling-related logic.

---
+++ lib/SILOptimizer/Utils/AutoDiffClosureSpecializationBridging.cpp
@@ -0,0 +1,24 @@
+//===--- AutoDiffClosureSpecializationBridging.cpp ------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+#define DEBUG_TYPE "autodiff-closure-specialization-bridging"
+
+#include "swift/SILOptimizer/AutoDiffClosureSpecializationBridging.h"
+
+using namespace swift;
+
+BridgedOwnedString getEnumDeclAsString(BridgedType bteType) {
+  std::string str;
+  llvm::raw_string_ostream out(str);
+  bteType.unbridged().getEnumOrBoundGenericEnum()->print(out);
+  return BridgedOwnedString(/*stringToCopy=*/StringRef(str));
+}
