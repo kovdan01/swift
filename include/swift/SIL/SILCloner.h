87a23cfffccd3eefcd6d1e4dd601bb9e515f74bd
Daniil Kovalev <dkovalev@accesssoftek.com>
2025-10-06 10:55:45 +0300

    remaining

--- include/swift/SIL/SILCloner.h
+++ include/swift/SIL/SILCloner.h
@@ -2294,6 +2294,10 @@ void SILCloner<ImplClass>::visitDestroyValueInst(DestroyValueInst *Inst) {
                   RefCountingInst::Atomicity::Atomic));
   }
 
+  if (getBuilder().getFunction().hasOwnership() &&
+      getOpValue(Inst->getOperand())->getOwnershipKind() == OwnershipKind::None)
+    return;
+
   recordClonedInstruction(Inst, getBuilder().createDestroyValue(
                                     getOpLocation(Inst->getLoc()),
                                     getOpValue(Inst->getOperand()),
