d298f24a65c5e1186026fec587937bbd6b6c02c1
Daniil Kovalev <dkovalev@accesssoftek.com>
2025-12-01 03:07:40 +0300

    Remaining

--- lib/Demangling/Demangler.cpp
+++ lib/Demangling/Demangler.cpp
@@ -3401,6 +3401,7 @@ NodePointer Demangler::demangleFunctionSpecialization() {
 
       auto ParamKind = (FunctionSigSpecializationParamKind)KindNd->getIndex();
       switch (ParamKind) {
+        case FunctionSigSpecializationParamKind::AutoDiffBranchTracingEnum:
         case FunctionSigSpecializationParamKind::ClosureProp: {
           while (NodePointer Ty = popNode(Node::Kind::Type)) {
             paramToAdd = addChild(paramToAdd, Ty);
@@ -3455,6 +3456,12 @@ NodePointer Demangler::demangleFuncSpecParam(Node::Kind Kind) {
       return addChild(Param, createNode(
          Node::Kind::FunctionSignatureSpecializationParamPayload, (Node::IndexType)prevArgIdx));
     }
+    case 'b':
+      return addChild(
+          Param,
+          createNode(Node::Kind::FunctionSignatureSpecializationParamKind,
+                     uint64_t(FunctionSigSpecializationParamKind::
+                              AutoDiffBranchTracingEnum)));
     case 'p': {
       for (;;) {
         switch (nextChar()) {
