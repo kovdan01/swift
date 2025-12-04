92c0e2091da633c1f2d16c51656e38b5f7fd917e
Daniil Kovalev <dkovalev@accesssoftek.com>
2025-12-04 15:17:32 +0300

    FULL ADCS initial

--- lib/Demangling/Demangler.cpp
+++ lib/Demangling/Demangler.cpp
@@ -3403,6 +3403,7 @@ NodePointer Demangler::demangleFunctionSpecialization() {
 
       auto ParamKind = (FunctionSigSpecializationParamKind)KindNd->getIndex();
       switch (ParamKind) {
+        case FunctionSigSpecializationParamKind::AutoDiffBranchTracingEnum:
         case FunctionSigSpecializationParamKind::ClosureProp: {
           while (NodePointer Ty = popNode(Node::Kind::Type)) {
             paramToAdd = addChild(paramToAdd, Ty);
@@ -3457,6 +3458,12 @@ NodePointer Demangler::demangleFuncSpecParam(Node::Kind Kind) {
       return addChild(Param, createNode(
          Node::Kind::FunctionSignatureSpecializationParamPayload, (Node::IndexType)prevArgIdx));
     }
+    case 'b':
+      return addChild(
+          Param,
+          createNode(Node::Kind::FunctionSignatureSpecializationParamKind,
+                     uint64_t(FunctionSigSpecializationParamKind::
+                              AutoDiffBranchTracingEnum)));
     case 'p': {
       for (;;) {
         switch (nextChar()) {
