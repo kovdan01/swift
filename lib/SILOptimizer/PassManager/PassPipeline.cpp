4a7a0096129169e481a1053dc497ae2198018942
Daniil Kovalev <dkovalev@accesssoftek.com>
2025-12-04 11:28:50 +0300

    FULL ADCS initial

--- lib/SILOptimizer/PassManager/PassPipeline.cpp
+++ lib/SILOptimizer/PassManager/PassPipeline.cpp
@@ -1015,7 +1015,7 @@ SILPassPipelinePlan::getPerformancePassPipeline(const SILOptions &Options) {
   if (SILPrintFinalOSSAModule) {
     addModulePrinterPipeline(P, "SIL Print Final OSSA Module");
   }
-  P.addAutodiffClosureSpecialization();
+  P.addAutodiffClosureSpecialization1();
 
   P.addOwnershipModelEliminator();
 
@@ -1023,7 +1023,7 @@ SILPassPipelinePlan::getPerformancePassPipeline(const SILOptions &Options) {
   // high-level constructs like @_semantics calls.
   addMidLevelFunctionPipeline(P);
 
-  P.addAutodiffClosureSpecialization();
+  P.addAutodiffClosureSpecialization2();
 
   // Perform optimizations that specialize.
   addClosureSpecializePassPipeline(P);
