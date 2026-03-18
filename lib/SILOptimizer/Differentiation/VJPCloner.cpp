2d983a0b118d62c68e31b2a58f04aae161c70cf7
Daniil Kovalev <dkovalev@accesssoftek.com>
2026-03-18 21:35:46 +0300

    Revert "x"
    This reverts commit 4fb8f60627e4ab8d9298cff7c2cab60363386095.

--- lib/SILOptimizer/Differentiation/VJPCloner.cpp
+++ lib/SILOptimizer/Differentiation/VJPCloner.cpp
@@ -1910,8 +1910,7 @@ SILFunction *VJPCloner::Implementation::createEmptyPullback() {
   auto pbGenericSig = vjp->getLoweredFunctionType()->getInvocationGenericSignature();
   auto *pbGenericEnv = pbGenericSig.getGenericEnvironment();
 
-  llvm::errs() << "createEmptyPullback invocation sig = " << pbGenericSig << "\n";
-  llvm::errs() << "createEmptyPullback generic sig = " << vjp->getLoweredFunctionType()->getSubstGenericSignature() << "\n";
+  llvm::errs() << "createEmptyPullback pbGenericSig = " << pbGenericSig << "\n";
 
   auto pbType = SILFunctionType::get(
       pbGenericSig, SILExtInfo::getThin(), origTy->getCoroutineKind(),
