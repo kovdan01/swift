27af01839122dda047e97fe97917cc56d0fd0497
Daniil Kovalev <dkovalev@accesssoftek.com>
2026-05-07 17:11:02 +0300

    Revert "[AutoDiff] Find `partial_apply` of pullback w/o loop in closure spec pass"
    This reverts commit 706e9e6ad643b7454a272b5788c8967f994468db.

--- SwiftCompilerSources/Sources/Optimizer/FunctionPasses/ClosureSpecialization.swift
+++ SwiftCompilerSources/Sources/Optimizer/FunctionPasses/ClosureSpecialization.swift
@@ -172,20 +172,15 @@ let autodiffClosureSpecialization = FunctionPass(name: "autodiff-closure-special
 
   repeat {
     var changed = false
-
-    guard let returnInst = function.returnInstruction,
-      let tupleInst = returnInst.returnedValue.definingInstruction as? TupleInst,
-      let lastTupleOp = tupleInst.operands.last,
-      let partialApplyInst = lastTupleOp.value.definingInstruction as? PartialApplyInst,
-      partialApplyInst.uses.singleUse?.instruction == tupleInst
-    else {
-      break
+    for inst in function.instructions {
+      if let partialApply = inst as? PartialApplyInst,
+         partialApply.isPullbackInResultOfAutodiffVJP
+      {
+        if trySpecialize(apply: partialApply, context) {
+          changed = true
+        }
+      }
     }
-
-    if trySpecialize(apply: partialApplyInst, context) {
-      changed = true
-    }
-
     if context.needFixStackNesting {
       context.fixStackNesting(in: function)
     }
@@ -774,6 +769,21 @@ private func numberOfIsolatedParameters(_ params: [ParameterInfo]) -> Int {
 }
 
 private extension PartialApplyInst {
+  /// True, if the closure obtained from this partial_apply is the
+  /// pullback returned from an autodiff VJP
+  var isPullbackInResultOfAutodiffVJP: Bool {
+    if self.parentFunction.isAutodiffVJP,
+      let use = self.uses.singleUse,
+      let tupleInst = use.instruction as? TupleInst,
+      let returnInst = self.parentFunction.returnInstruction,
+      tupleInst == returnInst.returnedValue
+    {
+      return true
+    }
+
+    return false
+  }
+
   var isPartialApplyOfThunk: Bool {
     if self.numArguments == 1,
       let fun = self.referencedFunction,
