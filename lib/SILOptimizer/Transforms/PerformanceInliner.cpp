5c6f6e8eb71ae228c089b17c3dfc02d3c393d434
Daniil Kovalev <dkovalev@accesssoftek.com>
2026-05-07 15:42:06 +0300

    change in inlining decisions

--- lib/SILOptimizer/Transforms/PerformanceInliner.cpp
+++ lib/SILOptimizer/Transforms/PerformanceInliner.cpp
@@ -431,7 +431,7 @@ bool SILPerformanceInliner::isTupleWithAllocsOrPartialApplies(SILValue val) {
 // TODO: VJPs of differentiable functions with custom silgen names are not
 // recognized as VJPs by this function. However, this is not a hard limitation
 // and can be fixed.
-bool isFunctionAutodiffVJP(SILFunction *callee) {
+static bool isFunctionAutodiffVJP(SILFunction *callee) {
   swift::Demangle::Context Ctx;
   if (auto *Root = Ctx.demangleSymbolAsNode(callee->getName())) {
     if (auto *node = Root->findByKind(
@@ -466,55 +466,55 @@ bool isAllocator(SILFunction *callee) {
   return false;
 }
 
-bool hasPullbackOnlyDirectUses(FullApplySite applySiteOfVJP) {
-  auto *applyOfVJP = dyn_cast<ApplyInst>(applySiteOfVJP.getInstruction());
-  if (applyOfVJP == nullptr)
-    return false;
+// static bool hasPullbackOnlyDirectUses(FullApplySite applySiteOfVJP) {
+//   auto *applyOfVJP = dyn_cast<ApplyInst>(applySiteOfVJP.getInstruction());
+//   if (applyOfVJP == nullptr)
+//     return false;
 
-  SILValue calleePullback = nullptr;
-  if (applyOfVJP->getType().isTuple()) {
-    auto numTupleElements = applyOfVJP->getType().getNumTupleElements();
-    assert(numTupleElements > 0);
-    for (auto user : applyOfVJP->getUsers()) {
-      if (auto *tei = dyn_cast<TupleExtractInst>(user)) {
-        if (tei->getFieldIndex() + 1 == numTupleElements) {
-          calleePullback = tei;
-          break;
-        }
-      } else if (auto *dti = dyn_cast<DestructureTupleInst>(user)) {
-        calleePullback = dti->getResult(numTupleElements - 1);
-        break;
-      } else {
-        return false;
-      }
-    }
-  } else {
-    calleePullback = applyOfVJP;
-  }
-  assert(calleePullback != nullptr);
-  assert(calleePullback->getType().isFunction());
+//   SILValue calleePullback = nullptr;
+//   if (applyOfVJP->getType().isTuple()) {
+//     auto numTupleElements = applyOfVJP->getType().getNumTupleElements();
+//     assert(numTupleElements > 0);
+//     for (auto user : applyOfVJP->getUsers()) {
+//       if (auto *tei = dyn_cast<TupleExtractInst>(user)) {
+//         if (tei->getFieldIndex() + 1 == numTupleElements) {
+//           calleePullback = tei;
+//           break;
+//         }
+//       } else if (auto *dti = dyn_cast<DestructureTupleInst>(user)) {
+//         calleePullback = dti->getResult(numTupleElements - 1);
+//         break;
+//       } else {
+//         return false;
+//       }
+//     }
+//   } else {
+//     calleePullback = applyOfVJP;
+//   }
+//   assert(calleePullback != nullptr);
+//   assert(calleePullback->getType().isFunction());
 
-  auto getSingleUser = [](SILValue val) -> SILInstruction * {
-    auto *use = val->getSingleUse();
-    return use ? use->getUser() : nullptr;
-  };
+//   auto getSingleUser = [](SILValue val) -> SILInstruction * {
+//     auto *use = val->getSingleUse();
+//     return use ? use->getUser() : nullptr;
+//   };
 
-  auto *pai = dyn_cast_or_null<PartialApplyInst>(getSingleUser(calleePullback));
-  if (!pai)
-    return false;
+//   auto *pai = dyn_cast_or_null<PartialApplyInst>(getSingleUser(calleePullback));
+//   if (!pai)
+//     return false;
 
-  auto *ti = dyn_cast_or_null<TupleInst>(getSingleUser(pai));
-  if (!ti)
-    return false;
+//   auto *ti = dyn_cast_or_null<TupleInst>(getSingleUser(pai));
+//   if (!ti)
+//     return false;
 
-  auto *ri = dyn_cast_or_null<ReturnInst>(getSingleUser(ti));
-  if (!ri)
-    return false;
+//   auto *ri = dyn_cast_or_null<ReturnInst>(getSingleUser(ti));
+//   if (!ri)
+//     return false;
 
-  return true;
-}
+//   return true;
+// }
 
-bool isTrivialVJP(SILFunction *vjp) {
+static bool isTrivialVJP(SILFunction *vjp) {
   auto getSingleUser = [](SILValue val) -> SILInstruction * {
     auto *use = val->getSingleUse();
     return use ? use->getUser() : nullptr;
@@ -548,15 +548,15 @@ bool isTrivialVJP(SILFunction *vjp) {
   return true;
 }
 
-bool isProfitableToInlineAutodiffVJP(FullApplySite applySite,
+static bool isProfitableToInlineAutodiffVJP(FullApplySite applySiteOfVJP,
                                      StringRef stageName) {
-  SILFunction *caller = applySite.getFunction();
-  SILFunction *callee = applySite.getReferencedFunctionOrNull();
-  assert(callee);
+  SILFunction *caller = applySiteOfVJP.getFunction();
+  SILFunction *calleeVJP = applySiteOfVJP.getReferencedFunctionOrNull();
+  assert(calleeVJP);
 
   bool isLowLevelFunctionPassPipeline = stageName == "LowLevel,Function";
   auto isCallerVJP = isFunctionAutodiffVJP(caller);
-  auto callerHasControlFlow = caller->size() > 1;
+  //auto callerHasControlFlow = caller->size() > 1;
 
   // If the pass is being run as part of the low-level function pass pipeline,
   // the autodiff closure-spec optimization is done doing its work. Therefore,
@@ -565,11 +565,12 @@ bool isProfitableToInlineAutodiffVJP(FullApplySite applySite,
     return true;
   }
 
-  if (callee->isThunk() == IsThunk_t::IsThunk) {
+  if (calleeVJP->isThunk() == IsThunk_t::IsThunk) {
     return true;
   }
 
-  if (isTrivialVJP(callee)) {
+  // Inlining of VJPs which are guaranteed to gain no optimization effect from ADCS does not harm ADCS effectiveness (we know in advance that it would not be effective). Always consider for inlining.
+  if (isTrivialVJP(calleeVJP)) {
     return true;
   }
 
@@ -577,21 +578,26 @@ bool isProfitableToInlineAutodiffVJP(FullApplySite applySite,
     return false;
   }
 
-  if (isCallerVJP && callerHasControlFlow) {
-    if (hasPullbackOnlyDirectUses(applySite))
-      return true;
+  // // Inlining of explicit VJP to corresponding derivative thunk does not harm ADCS effectiveness. Always consider for inlining.
+  // if (caller->isThunk() == IsThunk_t::IsThunk) {
+  //   return true;
+  // }
 
-    for (SILBasicBlock &bb : *caller) {
-      for (SILInstruction &inst : bb) {
-        if (auto *builtinInst = dyn_cast<BuiltinInst>(&inst)) {
-          if (builtinInst->getName().str() ==
-              "autoDiffProjectTopLevelSubcontext") {
-            return false;
-          }
-        }
-      }
-    }
-  }
+  // if (isCallerVJP && callerHasControlFlow) {
+  //   // if (hasPullbackOnlyDirectUses(applySiteOfVJP))
+  //   //   return true;
+
+  //   for (SILBasicBlock &bb : *caller) {
+  //     for (SILInstruction &inst : bb) {
+  //       if (auto *builtinInst = dyn_cast<BuiltinInst>(&inst)) {
+  //         if (builtinInst->getName().str() ==
+  //             "autoDiffProjectTopLevelSubcontext") {
+  //           return false;
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 
   return true;
 }
