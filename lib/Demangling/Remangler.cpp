5aed41225c5342f1dcc79ee31a4db2ffe064c200
Daniil Kovalev <dkovalev@accesssoftek.com>
2026-05-13 00:34:04 +0300

    ADCS full

--- lib/Demangling/Remangler.cpp
+++ lib/Demangling/Remangler.cpp
@@ -1621,6 +1621,9 @@ Remangler::mangleFunctionSignatureSpecializationParam(Node *node,
     case FunctionSigSpecializationParamKind::ClosurePropPreviousArg:
       Buffer << 'C' << node->getChild(idx++)->getIndex();
       break;
+    case FunctionSigSpecializationParamKind::AutoDiffBranchTracingEnum:
+      Buffer << 'b';
+      break;
     case FunctionSigSpecializationParamKind::BoxToValue:
       Buffer << 'i';
       break;
